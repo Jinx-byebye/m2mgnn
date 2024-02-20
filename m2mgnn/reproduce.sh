d3e1678621ceb181a9ef3c93febe4590c45fed0a
Jinx-byebye <148682360+Jinx-byebye@users.noreply.github.com>
2024-02-20 13:23:11 +0800

    Update

--- m2mgnn/reproduce.sh
+++ m2mgnn/reproduce.sh
@@ -7,3 +7,4 @@ python train.py --dataset 'chameleon' --beta 0.4 --hidden 64 --dropout 0. --nlay
 python train.py --dataset 'cora' --beta 0.6 --hidden 128 --dropout 0.7 --nlayers 2 --dropout2 0.7 --wd1 5e-4 --wd2 0.01 --lamda 0. --c 2 --lr 0.01  --temperature 0.5 --patience 100 --remove_self_loop False
 python train.py --dataset 'citeseer' --beta 0.5 --hidden 64 --dropout 0.4 --nlayers 2 --dropout2 0.3 --wd1 0.01 --wd2 5e-3 --lamda 0. --c 2 --lr 0.005  --temperature 1.0 --patience 100 --remove_self_loop False
 python train.py --dataset 'pubmed' --beta 0.8 --hidden 128 --dropout 0.4 --nlayers 2 --dropout2 0.3 --wd1 5e-4 --wd2 0.01 --lamda 0. --c 3 --lr 0.005  --temperature 2.0 --patience 100 --remove_self_loop False
+python train.py --dataset 'amazon-ratings' --beta 0.5 --hidden 256 --dropout 0.7 --nlayers 2 --dropout2 0.3 --wd1 5e-4 --wd2 5e-4 --lamda 0. --c 5 --lr 0.01  --temperature 1.0 --patience 200 --remove_self_loop False
